b88f4f490b46d51296d6969212c4793049ecd657
Crowdbotics <team@crowdbotics.com>
2021-09-05 04:36:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "patient_pond_30321Identifier",
+  url: "https://patient_pond_30321Identifier.botics.co",
+  api: "https://patient_pond_30321Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
